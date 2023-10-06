bc8f20188b238fc7360d8c68fc0693b2feaa25f8
Pratyush Dehury <98162702+pratyushd0308@users.noreply.github.com>
2023-10-06 11:56:31 +0530

    Added my awesome project in Java

---
+++ Java/Calculator.java
@@ -0,0 +1,30 @@
+
+
+public class Calculator {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java Calculator <operand1> <operator> <operand2>");
+            System.exit(1);
+        }
+
+        double operand1 = Double.parseDouble(args[0]);
+        double operand2 = Double.parseDouble(args[2]);
+
+        switch (args[1]) {
+            case "+":
+                System.out.println(operand1 + operand2);
+                break;
+            case "-":
+                System.out.println(operand1 - operand2);
+                break;
+            case "*":
+                System.out.println(operand1 * operand2);
+                break;
+            case "/":
+                System.out.println(operand1 / operand2);
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}
